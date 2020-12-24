d80f2d60424da3342d7615ac845d6a24f5dd4e6c
ilpersi <lpersichetti@gmail.com>
2020-12-24 01:05:31 +0100

    Raid button Cue for Unity

--- src/main/java/com/github/ilpersi/BHBot/CueManager.java
+++ src/main/java/com/github/ilpersi/BHBot/CueManager.java
@@ -613,6 +613,7 @@ class CueManager {
         // Main screen cues
         addCue("Main", "unitycues/mainScreen/cueMainScreen.png", new Bounds(100, 10, 110, 25)); // Gem cue used to identify the main screen
         addCue("SettingsGear", "unitycues/mainScreen/cueSettingsGear.png", new Bounds(655, 450, 730, 515)); // settings button
+        addCue("RaidButton", "unitycues/mainScreen/cueRaidButton.png", Bounds.fromWidthHeight(15, 200, 45, 200)); // Raid button
 
         // Blockers
         addCue("News", "unitycues/blockers/cueNewsPopup.png", Bounds.fromWidthHeight(345, 70, 110, 45)); // news popup
