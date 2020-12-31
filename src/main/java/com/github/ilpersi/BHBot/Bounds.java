67ea77d89ff834304954d956eb446388915182eb
ilpersi <lpersichetti@gmail.com>
2020-12-31 17:32:19 +0100

    New Bounds.fromMarvinSegment static method

--- src/main/java/com/github/ilpersi/BHBot/Bounds.java
+++ src/main/java/com/github/ilpersi/BHBot/Bounds.java
@@ -27,4 +27,35 @@ class Bounds {
     static Bounds fromWidthHeight(int x1, int y1, int width, int height) {
         return new Bounds(x1, y1, x1+width, y1+height);
     }
+
+    /**
+     * This method will create a Bounds object starting from a seg. This can be useful
+     * at development time and in situations where you wat to get the Bounds in a fast
+     * way without having to manually calculate them
+     *
+     * @param seg The starting MarvinSegment
+     * @param margin The desired margin from the seg, if null it will default to 10
+     * @return A Bounds object containing the seg
+     */
+    static Bounds fromMarvinSegment(MarvinSegment seg, Integer margin) {
+        if (margin == null) margin = 10;
+
+        int suggestedX1 = seg.x1 - margin;
+        while ((suggestedX1 % 5) != 0) suggestedX1 -= 1;
+
+        int suggestedY1 = seg.y1 - margin;
+        while ((suggestedY1 % 5) != 0) suggestedY1 -= 1;
+
+        int suggestedWidth = seg.x2 - suggestedX1 + margin;
+        while ((suggestedWidth % 5) != 0) suggestedWidth += 1;
+
+        int suggestedHeight = seg.y2 - suggestedY1 + margin;
+        while ((suggestedHeight % 5) != 0) suggestedHeight += 1;
+
+        // we make sure not to exceed initial boundaries
+        if (suggestedX1 < 0) suggestedX1 = 0;
+        if (suggestedY1 < 0) suggestedY1 = 0;
+
+        return fromWidthHeight(suggestedX1, suggestedY1, suggestedWidth, suggestedHeight);
+    }
 }
