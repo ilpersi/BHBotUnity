0400a2dacc26d393498736df462f971fe54eebf1
ilpersi <lpersichetti@gmail.com>
2020-12-31 19:57:27 +0100

    New simple class to test Cues

---
+++ src/main/java/com/github/ilpersi/BHBot/CueTest.java
@@ -0,0 +1,21 @@
+package com.github.ilpersi.BHBot;
+
+public class CueTest {
+    public static void main(String[] args) {
+        String usage = "CueTest <imageToSearchPat> <cueImagePath>";
+
+        if (args.length <2) {
+            System.out.println(usage);
+        } else {
+            MarvinSegment seg = MarvinSegment.fromFiles(args[0], args[1]);
+
+            if (seg != null) {
+                Bounds suggestedBounds = Bounds.fromMarvinSegment(seg, null);
+
+                System.out.println("Suggested Bounds: " + suggestedBounds.getJavaCode(true, false));
+            } else {
+                System.out.println("Sub image not found.");
+            }
+        }
+    }
+}
