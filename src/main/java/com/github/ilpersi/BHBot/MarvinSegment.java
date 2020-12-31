e76e629a0700e871872646a9cfee8d52153551eb
ilpersi <lpersichetti@gmail.com>
2020-12-31 17:36:25 +0100

    New MarvinSegment.fromFiles

--- src/main/java/com/github/ilpersi/BHBot/MarvinSegment.java
+++ src/main/java/com/github/ilpersi/BHBot/MarvinSegment.java
@@ -1,7 +1,10 @@
 package com.github.ilpersi.BHBot;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashSet;
 
 /**
@@ -181,6 +184,49 @@ public class MarvinSegment {
 
     static MarvinSegment fromCue(String cueName, int timeout, BrowserManager browserManager) {
         return fromCue(BHBot.cues.get(cueName), timeout, true, browserManager);
+    }
+
+    /**
+     * This method will return a MarviSegment starting from two image files. Use this
+     * method during development time to check that the Cues you created works fine.
+     *
+     * @param imageInFile The main image that will be used to search the Cue
+     * @param subImageFile The Cue image you are looking for
+     * @return The first found Marvin Segment
+     */
+    static MarvinSegment fromFiles(File imageInFile, File subImageFile) {
+        BufferedImage imageIn = null, subImage = null;
+
+        try {
+            imageIn = ImageIO.read(imageInFile);
+        } catch (IOException e) {
+            BHBot.logger.error("Error when loading image file.", e);
+        }
+
+        try {
+            subImage = ImageIO.read(subImageFile);
+        } catch (IOException e) {
+            BHBot.logger.error("Error when loading sub-image file.", e);
+        }
+
+        return FindSubimage.findImage(imageIn, subImage, 0, 0, 0, 0);
+    }
+
+    static  MarvinSegment fromFiles(String imageInPath, String subImagePath) {
+        File imageInFile = new File(imageInPath);
+        File subImageFile = new File(subImagePath);
+
+        if (!imageInFile.exists()) {
+            BHBot.logger.error("Image in file does not exist.");
+            return null;
+        }
+
+        if (!subImageFile.exists()) {
+            BHBot.logger.error("Sub-image file does not exist.");
+            return null;
+        }
+
+        return fromFiles(imageInFile, subImageFile);
     }
 
     public String toString() {
