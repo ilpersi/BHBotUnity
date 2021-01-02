7af7b800d7967c0ec6c30c08a79b778fbfdf5fc6
ilpersi <lpersichetti@gmail.com>
2021-01-02 16:50:11 +0100

    CueTest improvements

--- src/main/java/com/github/ilpersi/BHBot/CueTest.java
+++ src/main/java/com/github/ilpersi/BHBot/CueTest.java
@@ -7,11 +7,14 @@ public class CueTest {
         if (args.length <2) {
             System.out.println(usage);
         } else {
+            System.out.println("Main image: " + args[0]);
+            System.out.println("Cue  image: " + args[1]);
+
             MarvinSegment seg = MarvinSegment.fromFiles(args[0], args[1]);
 
+            System.out.println("");
             if (seg != null) {
                 Bounds suggestedBounds = Bounds.fromMarvinSegment(seg, null);
-
                 System.out.println("Suggested Bounds: " + suggestedBounds.getJavaCode(true, false));
                 System.out.println("Suggested Bounds.fromWidthHeight: " + suggestedBounds.getJavaCode(true, true));
             } else {
